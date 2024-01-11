f18fb9b8d89e02bfbde16c03f674380345546350
MariaKonstantinov <konstantinovamariia1@gmail.com>
2024-01-11 15:31:28 +0200

    stage-3: edit newscard.js

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,51 +1,3 @@
-// import React, { useState } from "react";
-// import "./NewsCard.css";
-
-// const NewsCard = ({ data, onSavedArticlesPage, loggedIn }) => {
-//   const [isSaved, setIsSaved] = useState(false);
-
-//   function handleSave() {
-//     setIsSaved(!isSaved);
-//   }
-
-//   return (
-//     <div className="news-card">
-//       <img className="news-card__image" src={data.image} alt={data.alt} />
-//       {!onSavedArticlesPage && (
-//         <>
-//           <button
-//             className={`news-card__button news-card__button_save ${
-//               loggedIn && isSaved ? "news-card__button_save_active" : ""
-//             }`}
-//             onClick={handleSave}
-//           ></button>
-//           {!loggedIn && (
-//             <div className="news-card__tag news-card__tag_type_tooltip">
-//               Sign in to save articles
-//             </div>
-//           )}
-//         </>
-//       )}
-//       {onSavedArticlesPage && (
-//         <>
-//           <button className="news-card__button news-card__button_remove"></button>
-//           <div className="news-card__tag news-card__tag_type_keyword">
-//             {data.keyword}
-//           </div>
-//         </>
-//       )}
-//       <div className="news-card__details">
-//         <p className="news-card__date">{data.date}</p>
-//         <h2 className="news-card__title">{data.title}</h2>
-//         <p className="news-card__description">{data.description}</p>
-//         <p className="news-card__source">{data.source}</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default NewsCard;
-
 import React, { useState, useEffect } from "react";
 import "./NewsCard.css";
 
@@ -60,7 +12,7 @@ const NewsCard = ({
 }) => {
   const [isSaved, setIsSaved] = useState(false);
 
-  /* Checks if the searched cards are saved by checking if the titles 
+  /* checks if the searched cards are saved by checking if the titles 
   are matching. If there is a match, then the state become isSaved 
   which turns into marked blue icon  */
   useEffect(() => {
@@ -140,7 +92,7 @@ const NewsCard = ({
           if (loggedIn) {
             handleSave(data);
           } else {
-            /* If the user is not logged in, then the sign in popup 
+            /* If the user is not logged in, the sign in popup 
           will open when clicking to save */
             onSignInClick();
           }
